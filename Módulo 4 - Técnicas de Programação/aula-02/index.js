d883395451cbf7a70f72c5225bd291b743b4d577
Artur Faiotto Clemente <artur.faiotto@gmail.com>
2022-02-16 11:17:17 -0300

    modif index

--- Módulo 4 - Técnicas de Programação/aula-02/index.js
+++ Módulo 4 - Técnicas de Programação/aula-02/index.js
@@ -1,5 +1,5 @@
 function InfoPessoal(nome, sobrenome, idade) {
-    return console.log({ nome, sobrenome, idade });
+	return console.log({ nome, sobrenome, idade });
 }
 
 InfoPessoal('Artur', 'Clemente', 30);
